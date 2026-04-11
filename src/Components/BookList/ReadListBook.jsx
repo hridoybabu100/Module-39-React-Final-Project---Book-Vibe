0302b6996a7c8865094f0c8a2a23113ab5c6c7dd
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-04-11 14:50:38 +0600

    all projected updated

--- src/Components/BookList/ReadListBook.jsx
+++ src/Components/BookList/ReadListBook.jsx
@@ -1,11 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { BookContext } from "../../Context/Bookprovider";
 import BookCard from "../UI/BookCard";
 
-const ReadListBook = () => {
+const ReadListBook = ({sortType}) => {
   const { storeBooks } = useContext(BookContext);
 
-  if (storeBooks.length === 0) {
+  const [filter, setFilter] = useState(storeBooks);
+
+  useEffect(() => {
+    if (sortType){
+      if(sortType === "pages"){
+        const sortData = [...storeBooks].sort((a,b) => a.totalPages - b.totalPages);     
+        console.log(sortData);
+        setFilter(sortData)
+        
+      }
+      else if ( sortType === 'rating'){
+        const sortData = [...storeBooks].sort((a,b) => a.rating - b.rating); 
+        setFilter(sortData)
+      }
+    }
+
+  },[sortType, storeBooks])
+
+  if (filter.length === 0) {
     return (
       <div className="flex justify-center items-center bg-zinc-300 h-[60vh] rounded-2xl">
         <h1 className="text-4xl font-bold">No Read Book Lis Found Hare</h1>
@@ -15,7 +33,7 @@ const ReadListBook = () => {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {storeBooks.map((book) => (
+        {filter.map((book) => (
           <BookCard key={book.id} book={book}></BookCard>
         ))}
       </div>
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,20 +1,36 @@
-import React, { useContext } from "react";
-import { BookContext } from "../../Context/Bookprovider";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import BookCard from "../../Components/UI/BookCard";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import ReadListBook from "../../Components/BookList/ReadListBook";
 import WishListBook from "../../Components/BookList/WishListBook";
+import { useState } from "react";
 
 const Books = () => {
-  
   // console.log(storeBooks, wishListBooks, "Api context list in data");
+
+  const [sortType , setSortType] = useState();
+  // console.log("Sorting Type", sortType);
+  
 
   return (
     <div className="mx-auto w-[85%] py-10">
-      
-      
-
+      <div className="flex justify-center items-center py-4">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Sort By⬇️
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li onClick={() => setSortType("pages")}>
+              <a>Pages</a>
+            </li>
+            <li onClick={() => setSortType("rating")}>
+              <a>Rating</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <Tabs>
         <TabList>
           <Tab>Read List</Tab>
@@ -22,10 +38,10 @@ const Books = () => {
         </TabList>
 
         <TabPanel>
-          <ReadListBook></ReadListBook>
+          <ReadListBook sortType={sortType}></ReadListBook>
         </TabPanel>
         <TabPanel>
-          <WishListBook></WishListBook>
+          <WishListBook sortType={sortType}></WishListBook>
         </TabPanel>
       </Tabs>
     </div>
